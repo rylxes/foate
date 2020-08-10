ed6be60cab4994498b93d95a79f1932de5dd5f5e
ben4code <nnaemeka.obioha@gmail.com>
2020-08-10 04:17:23 +0100

    Changed font-family and font-weight and

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,8 @@ export default function index() {
   return (
     <div>
       <Home/>
-      {/* <pre>{JSON.stringify(users, null, 4)}</pre> */}
     </div>
   )
 }
 
 
-// export const getServerSideProps = async ( ) => {
-//   const res = await fetch('http://localhost:3000/api/users');
-//   const users = await res.json();
-//   console.log('api: ', users)
-//   return {
-//     props: {users}
-//   }
-// }
